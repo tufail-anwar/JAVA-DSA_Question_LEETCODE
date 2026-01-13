283a873a906f358fdaf18315849af275327d47ed
dildarshaikh-stack <dildarmr887@gmail.com>
2026-01-13 22:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (14.69%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right){
+            return head;
+        }
+    ListNode pres=head;
+        ListNode prev=null;
+        for(int i=0;pres !=null && i<left-1;i++){
+            prev=pres;
+            pres=pres.next;
+        }
+    ListNode last=prev;
+    ListNode newEnd=pres;
+
+    ListNode Next=pres.next;
+    for (int i = 0; pres !=null && i <right-left+1 ; i++) {
+        pres.next=prev;
+        prev=pres;
+        pres=Next;
+        if(Next!=null){
+            Next=Next.next;
+        }
+    }
+    if(last!=null){
+        last.next=prev;
+    }else{
+        head=prev;
+    }
+    newEnd.next=pres;
+    return head;
+    }
+}
