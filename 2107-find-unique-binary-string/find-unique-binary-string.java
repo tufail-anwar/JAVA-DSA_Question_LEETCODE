006033f4bc61061c4ff260cead8e9a10c73bd249
MD TUFAIL ANWAR <dildarmr887@gmail.com>
2026-03-08 23:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (64.70%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i].charAt(i) == '0')
+                ans.append('1');
+            else
+                ans.append('0');
+        }
+        return ans.toString();
+    }
+}
