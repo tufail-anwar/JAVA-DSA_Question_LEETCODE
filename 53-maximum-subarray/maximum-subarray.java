24c287630cbf494d3cdbae74e1a6bf42eb76717c
MD TUFAIL ANWAR <dildarmr887@gmail.com>
2026-03-09 23:46:35 +0530

    Time: 1 ms (99.82%) | Memory: 77 MB (91.60%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum>max){
+                max = Math.max(sum,max);
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+}
